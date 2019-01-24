534e1953320fae1de5ce5ab2dab195201860fa6a
sahil <sahil@localhost.localdomain>
2019-01-24 08:51:00 -0800

    area of circle,tringle,rextangle with the help of function

---
+++ fun_area.py
@@ -0,0 +1,16 @@
+def circle(r,h):
+     a=3.14*r*r*h
+     print 'area of circle is:',a
+
+def tringle(l,h):
+    a=0.5*l*h
+    print 'area of tringle is:',a
+
+def rectangular(l,b):
+    a=l*b
+    print 'area of rectangular is:',a
+
+circle(3,4)
+tringle(4,3)
+rectangular(5,4)
+
