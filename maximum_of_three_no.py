816994d2f556253b91b65664fda8c08bf0ebf2df
sahil <sahil@localhost.localdomain>
2019-02-16 22:15:39 -0800

    maximum of three no

---
+++ maximum_of_three_no.py
@@ -0,0 +1,11 @@
+a=input("enter the 1st no")
+b=input("enter the 2nd no")
+c=input("enter the 3rd no")
+
+if a>b & a>c:
+   print("a is max"),a
+elif b>a & b>c:
+   print("b is max"),b
+else:
+   print("c is max"),c
+
